d22b8ab1873c06dcf87d6392aa4b0393620e7ccc
Lukas Hajdu <lukas@loftdigital.com>
2015-11-09 16:00:07 +0000

    RSS-68 Load fixture settings from config file

--- Test/WebTestCase.php
+++ Test/WebTestCase.php
@@ -4,6 +4,7 @@ namespace LoftDigital\RestBundle\Test;
 
 use Symfony\Bundle\FrameworkBundle\Client;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase as FrameworkWebTestCase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\Response;
 
 /**
@@ -35,6 +36,9 @@ abstract class WebTestCase extends FrameworkWebTestCase
     /** @var Client */
     protected $client;
 
+    /** @var ContainerInterface */
+    protected $container;
+
     const ACCEPT_JSON_V1_0 = 'application/json;version=1.0';
     const ACCEPT_XML_V1_0 = 'application/xml;version=1.0';
 
@@ -54,7 +58,9 @@ abstract class WebTestCase extends FrameworkWebTestCase
      */
     public function setUp()
     {
-        $this->token = 'd43Gekjf73k0k02djh';
+        $this->container = static::createClient()->getKernel()->getContainer();
+        $user = $this->container->getParameter('loft_digital_rest_oauth.user');
+        $this->token = $user['token'];
     }
 
     /**
@@ -90,6 +96,14 @@ abstract class WebTestCase extends FrameworkWebTestCase
     public function getCrawler()
     {
         return $this->client->getCrawler();
+    }
+
+    /**
+     * @return ContainerInterface
+     */
+    public function getContainer()
+    {
+        return $this->container;
     }
 
     /**
