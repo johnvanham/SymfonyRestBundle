00380226871b8111d7ae99b625ccbc9dbb500095
Lukas Hajdu <lukas@loftdigital.com>
2016-01-05 12:30:36 +0100

    RSS-53 Remove duplicate function call

--- Entity/EntityRepository.php
+++ Entity/EntityRepository.php
@@ -2,6 +2,7 @@
 
 namespace LoftDigital\RestBundle\Entity;
 
+use Doctrine\Common\Collections\Criteria;
 use Doctrine\ORM\EntityRepository as GenericEntityRepository;
 use Rss\UserApiBundle\Entity\User;
 
@@ -16,6 +17,15 @@ class EntityRepository extends GenericEntityRepository
 {
     /** @var User */
     protected $user;
+
+    /** @var array */
+    protected $allowedStores = [];
+
+    /** @var string */
+    protected $orderBy;
+
+    /** @var string */
+    protected $order = Criteria::ASC;
 
     /**
      * Set user
@@ -42,6 +52,10 @@ class EntityRepository extends GenericEntityRepository
             return [];
         }
 
+        if (!empty($this->allowedStores)) {
+            return $this->allowedStores;
+        }
+
         $userStores = $this->getEntityManager()
             ->createQuery('
                 SELECT u2s.storeId
@@ -62,10 +76,38 @@ class EntityRepository extends GenericEntityRepository
             ->getResult()
         ;
 
-        $stores = array_unique(array_map(function ($item) {
+        $this->allowedStores = array_unique(array_map(function ($item) {
             return $item['storeId'];
         }, array_merge($userStores, $userBrandStores)));
 
-        return $stores;
+        return $this->allowedStores;
+    }
+
+    /**
+     * Set order by
+     *
+     * @param string $orderBy
+     *
+     * @return $this
+     */
+    public function setOrderBy($orderBy)
+    {
+        $this->orderBy = $orderBy;
+
+        return $this;
+    }
+
+    /**
+     * Set order
+     *
+     * @param string $order
+     *
+     * @return $this
+     */
+    public function setOrder($order)
+    {
+        $this->order = $order;
+
+        return $this;
     }
 }
