6a05db666e77a9242935845a4459399f2431ec58
Lukas Hajdu <lukas@loftdigital.com>
2016-02-02 15:10:00 +0000

    RSS-134 Event socket handling

---
+++ RPC/AbstractRPC.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace LoftDigital\RestBundle\RPC;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * Class AbstractRPC
+ *
+ * @author Tomas Lang <tomas@loftdigital.com>
+ * @copyright Loft Digital <www.loftdigital.com>, 2015
+ * @package Rss\EnquiryApiBundle\RPC
+ */
+abstract class AbstractRPC extends Controller
+{
+    /**
+     * Publish message to channel
+     *
+     * @param string $uri
+     * @param array $data
+     */
+    protected function publish($uri, array $data)
+    {
+        $this->get('thruway.client')->publish(
+            $uri,
+            [$data],
+            [],
+            ['acknowledge' => true]
+        )->then(
+            function () {
+            },
+            function ($error) {
+                $this->get('logger')->info(json_encode($error));
+            }
+        );
+    }
+}
