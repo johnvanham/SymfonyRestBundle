b68a4428c822c78048811f9d43f673de705ecb47
Lukas Hajdu <lukas@loftdigital.com>
2016-02-09 11:32:23 +0000

    RSS-140 Add event reports

---
+++ Handler/UnsupportedReportTypeException.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace LoftDigital\RestBundle\Handler;
+
+use Exception;
+
+/**
+ * Class UnsupportedReportTypeException
+ *
+ * @author Lukas Hajdu <lukas@loftdigital.com>
+ * @copyright Loft Digital <http://weareloft.com>, 2016
+ * @package LoftDigital\RestBundle\Handler
+ */
+class UnsupportedReportTypeException extends Exception
+{
+}
