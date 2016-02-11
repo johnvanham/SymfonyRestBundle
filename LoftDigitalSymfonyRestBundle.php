c02e43719f335fb3a55134667aaf1775aa1c774f
Lukas Hajdu <lukas@loftdigital.com>
2016-02-11 13:48:30 +0000

    Rename bundle to follow convention

---
+++ LoftDigitalSymfonyRestBundle.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace LoftDigital\SymfonyRestBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Loft Digital Symfony REST Bundle
+ *
+ * @author Lukas Hajdu <lukas@loftdigital.com>
+ * @copyright Loft Digital <http://weareloft.com>, 2016
+ * @package LoftDigital\SymfonyRestBundle
+ */
+class LoftDigitalSymfonyRestBundle extends Bundle
+{
+}
